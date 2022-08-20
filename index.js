9cdec8e2563080f105fe840b595def954b0d3ebe
thadeus <thadeusnyariki94@gmail.com>
2022-08-20 14:51:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => titleCase(tutorial));
 }
+  function titleCase(str) {
+  return str.split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
+}
+
